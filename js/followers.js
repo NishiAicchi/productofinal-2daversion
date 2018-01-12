ad16de6de629abeb11026e44b39e9c53e6d0235e
NishiAicchi <ai.espichan@gmail.com>
2018-01-12 12:57:33 -0500

    añadiendo funcionalidad a vistas

--- js/followers.js
+++ js/followers.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   console.log(data);
 
+  $back = $('#arrow-back');
   $username = $('#username');
   $avatar = $('avatar');
   $followBtn = $('follow-btn');
 
-  $username.load(data, )
+  // evento que nos regresa a la opcion anterior
+ 
+  $back.click(function(e) {
+    // console.log($(this).val());
+    window.location.href = 'profile.html';
+  });
+
 });
--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  // evento que cambia el estado de la insignia
+  $('#switch').click(function(e) {
+    if (event.target.checked === true) {
+      $('#status').text('Me Adoptaron');
+    } else if (event.target.checked === false) {
+      $('#status').text('Busco un hogar');
+    }
+  });
 
+  // evento que regresa a la vista anterior
+  $('#arrow-back').click(function() {
+    window.location.href = 'homepage.html';
+  }),
+  
   $('#textarea-1').focus();
   /* Publicar post - Al presionar #btn-post se publica el comentario*/
-  $('#btn-post').click(function () {
+  $('#btn-post').click(function() {
     event.preventDefault();
     var post = $('#textarea-1').val();
     $('#publication').append('<div class="card">' + '<div class ="avatar-container-2">' + '<img src = "../assets/images/dog-perfil.jpg" class="circle avatar-xs">' + '<span>' + '<p> Huellitas </p>' + '</span>' + '</div>' + '<div class="card-content wrap">' + '<p>' + post + '<p>' + '</div>' + '</div>');
@@ -12,28 +25,24 @@ $(document).ready(function () {
 
   $('.modal').modal();
 
-  $('#btn-addimg').click(function () {
+  $('#btn-addimg').click(function() {
     event.preventDefault();
 
     $('#file').hide();
 
-    $('input[type=file]').change(function () {
+    $('input[type=file]').change(function() {
       var file = (this.files[0].name).toString();
       var reader = new FileReader();
 
       $('#file-path').text('');
       $('#file-path').text(file);
 
-      reader.onload = function (e) {
+      reader.onload = function(e) {
         $('#file-path').append('src', e.target.result);
         $('#publication').append('<div class="card">' + '<div class ="avatar-container-2">' + '<img src = "../assets/images/dog-perfil.jpg" class="circle avatar-xs">' + '<span>' + '<p> Huellitas </p>' + '</span>' + '</div>' + '<div class="card-content wrap">' + '<p>' + post + '<p>' + '</div>' + '</div>');
-
-      }
+      };
 
       reader.readAsDataURL(this.files[0]);
     });
-
-
-
   });
 });
